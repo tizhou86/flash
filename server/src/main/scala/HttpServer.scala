e3181f4149a2c5b5ab24650d12668be015a30930
Ti Zhou <ti.zhou@edifecs.com>
2014-12-24 15:37:40 -0800

    flash: Implemented basic akka http server.
    Implemented basic akka http server.
    Seems to be it still has long way to go.
    Will see how it combines with spray io.

---
+++ server/src/main/scala/HttpServer.scala
@@ -0,0 +1,23 @@
+/**
+ * Created by tiiizhou on 12/24/14.
+ */
+
+import akka.actor.ActorSystem
+import akka.http.Http
+import akka.stream.FlowMaterializer
+
+class HttpServer(host: String, port: Int) {
+
+  implicit val system = ActorSystem()
+  implicit val materializer = FlowMaterializer()
+
+  val serverBinding = Http(system).bind(interface = host, port = port)
+  val requestHandler = new RequestHandler().requestHandler
+
+  serverBinding.connections.foreach { connection =>
+    println(s"Accepted new connection from ${connection.remoteAddress}")
+
+    connection.handleWithSyncHandler(requestHandler)
+  }
+
+}
---
+++ server/src/main/scala/RequestHandler.scala
@@ -0,0 +1,26 @@
+import akka.http.model._
+import akka.http.model.HttpMethods._
+import akka.stream.scaladsl.Flow
+
+/**
+ * Created by tiiizhou on 12/24/14.
+ */
+class RequestHandler {
+
+  //This is only for proof of concept, will keep track of akka http for updating with routing dsl.
+  val requestHandler: HttpRequest => HttpResponse = {
+    case HttpRequest(GET, Uri.Path("/"), _, _, _) => {
+      HttpResponse(
+        entity = HttpEntity(MediaTypes.`text/html`,
+          "<html><body>Hello World</body></html>"))
+    }
+    case HttpRequest(GET, Uri.Path("/ping"), _, _, _) => {
+      HttpResponse(entity = "pong")
+    }
+    case HttpRequest(GET, Uri.Path("/crash"), _, _, _) => {
+      sys.error("Crashed!")
+    }
+    case _ => HttpResponse(404, entity = "Unknown Resources")
+  }
+
+}
